f3f326c96068b10955dbb15550c7dac143602daa
aahilalwani25 <alwani.aahil25@gmail.com>
2023-02-14 11:48:36 +0500

    initial commit

--- lib/Models/Medicines.dart
+++ lib/Models/Medicines.dart
@@ -1,6 +1,7 @@
 class Medicines {
-  String medicine_name, medicine_type, for_illness, currency;
+  String medicine_name, medicine_type, for_illness, currency, time;
   int price, available;
+
 
   Medicines({
     required this.medicine_name,
@@ -9,5 +10,6 @@ class Medicines {
     required this.available,
     required this.for_illness,
     required this.currency,
+    required this.time,
   });
 }
